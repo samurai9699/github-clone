2321f6e24cd2604e1a70a2f5b0992dba9fefdf65
sam muriithi <samuelmuriithi965@gmail.com>
2024-02-26 23:27:02 +0300

    backend starter

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,23 +1,26 @@
 import { Link } from "react-router-dom";
 import { IoHomeSharp } from "react-icons/io5";
 import { FaHeart } from "react-icons/fa";
-import {MdOutlineExplore} from "react-icons/md";
-import {PiSignInBold} from "react-icons/pi";
+import { MdOutlineExplore } from "react-icons/md";
+import { PiSignInBold } from "react-icons/pi";
 import { MdEditDocument } from "react-icons/md";
-import Logout from "./LogOut";
-
+import Logout from "./Logout";
+import { useAuthContext } from "../context/AuthContext";
 
 const Sidebar = () => {
-    const authUser = true;
-  return (
-    <aside className="flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r  bg-glass">
-    
-    <nav className="h-full flex flex-col gap-3">
-        <Link to="/" className="flex justify-center">
-            <img className="h-8" src="/github.svg" alt="github logo"/>
-        </Link>
+	const { authUser } = useAuthContext();
 
-        <Link
+	return (
+		<aside
+			className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8
+      overflow-y-auto border-r bg-glass'
+		>
+			<nav className='h-full flex flex-col gap-3'>
+				<Link to='/' className='flex justify-center'>
+					<img className='h-8' src='/github.svg' alt='Github Logo' />
+				</Link>
+
+				<Link
 					to='/'
 					className='p-1.5 flex justify-center transition-colors duration-200 rounded-lg 
 					hover:bg-gray-800'
@@ -59,15 +62,15 @@ const Sidebar = () => {
 					>
 						<MdEditDocument size={25} />
 					</Link>
-         )}
-         {authUser && (
+				)}
+
+				{authUser && (
 					<div className='flex flex-col gap-2 mt-auto'>
 						<Logout />
 					</div>
 				)}
-    </nav>
-    </aside>
-  )
-}
-
-export default Sidebar
+			</nav>
+		</aside>
+	);
+};
+export default Sidebar;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,24 +1,26 @@
 import { FaGithub, FaUnlockAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { handleLoginWithGithub } from "../lib/function";
 
 const SignUpPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
-        <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-glass">
-          <div className="p-6 spacey-y-4 md:space-y-6 sm:p-8">
-          <h1 className='text-xl font-bold md:text-2xl text-center'>Create Account</h1>
+	return (
+		<div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0'>
+			<div className='w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-glass'>
+				<div className='p-6 spacey-y-4 md:space-y-6 sm:p-8'>
+					<h1 className='text-xl font-bold md:text-2xl text-center'>Create Account</h1>
 
-          <button
-              type='button'
-              className='text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4
-            focus:ring-[#24292F]/50 font-medium rounded-lg flex gap-2 p-2 items-center w-full 
-              text-center justify-center'
-            >
-           <FaGithub className='w-5 h-5' />
-             Sign up with Github
-         </button>
+					<button
+						type='button'
+						className='text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4
+						focus:ring-[#24292F]/50 font-medium rounded-lg flex gap-2 p-2 items-center w-full 
+						text-center justify-center'
+						onClick={handleLoginWithGithub}
+					>
+						<FaGithub className='w-5 h-5' />
+						Sign up with Github
+					</button>
 
-         <p className='text-gray-300'>
+					<p className='text-gray-300'>
 						By signing up, you will unlock all the features of the app.
 						<span>
 							<FaUnlockAlt className='w-4 h-4 inline mx-2' />
@@ -31,10 +33,9 @@ const SignUpPage = () => {
 							Login
 						</Link>
 					</p>
-           </div>
-         </div>
-    </div>
-  )
-}
-
-export default SignUpPage
+				</div>
+			</div>
+		</div>
+	);
+};
+export default SignUpPage;
